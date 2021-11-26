70e5e50d45bbeb698a7a6ed6520f20b582c59138
Aaron Nguyen <me@aaronnguyen.dev>
2021-11-25 21:41:42 -0500

    Connect to Postgres thru TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'iluvcoffee',
+      autoLoadEntities: true,
+      synchronize: true,
+      // ^ Disable in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
